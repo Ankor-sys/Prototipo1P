a32aaa6d8bc2ffb76fcbd7061f307f7a9c6b525c
Sebas-Sept <sebas.ulugrun@outlook.com>
2020-08-28 15:01:45 -0600

    Creacion de un usuario para pruebas

--- ScriptParaLLenarTablas.sql
+++ ScriptParaLLenarTablas.sql
@@ -1,10 +1,12 @@
 use multibodegas;
 
-#insert into proveedores values("1", "ByBCompany", "58585858");
-#insert into clientes values("1", "Juanito", "45464545");
+insert into proveedores values("1", "ByBCompany", "58585858");
+insert into clientes values("1", "Juanito", "45464545");
+insert into Seguridad values("admin", "admin");
 #select * from productos;
 select * from inventarios;
 select * from ventas;
 select * from compras;
+select * from Seguridad;
 
 delete from ventas where id_venta = 1;
